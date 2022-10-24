7a48da01eea3ba0c016079563bc4b59a63176465
amStevee <stevenanongo2@gmail.com>
2022-10-24 14:37:47 -0700

    tsx-nav-&-footer min

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -6,7 +7,7 @@ import HotelList from "./pages/HotelList";
 function App() {
   return (
     <Routes>
-      <Route path="/" exact element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/hotels" element={<HotelList />} />
     </Routes>
   );
